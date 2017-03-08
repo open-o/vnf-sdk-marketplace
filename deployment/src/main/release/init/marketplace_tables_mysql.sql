e16d5b05e6b9855768f14160271cc3a83aaf3d06
sukesh a c <sukeshac@huawei.com>
2017-03-08 12:48:16 +0000

    market place deployment changes
    issue-id: VNFSDK-16
    Change-Id: I5c5bc7546b2220d541426e2d0b22f505bedce312
    Signed-off-by: sukesh a c <sukeshac@huawei.com>

---
+++ deployment/src/main/release/init/marketplace_tables_mysql.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE marketplaceDB;
+DROP TABLE IF EXISTS csar_package_table;
+
+CREATE TABLE csar_package_table (
+	CSARID                   VARCHAR(200)       NOT NULL,	
+	DOWNLOADURI              VARCHAR(200)       NULL,
+	REPORT		             VARCHAR(200)       NULL,
+	SIZE                     VARCHAR(100)       NULL,
+	FORMAT                   VARCHAR(100)       NULL,
+	CREATETIME               VARCHAR(100)       NULL,
+	DELETIONPENDING          VARCHAR(100)       NULL,
+	MODIFYTIME               VARCHAR(100)       NULL,
+	SHORTDESC	             TEXT		        NULL,
+	NAME                     VARCHAR(100)       NULL,
+	VERSION                  VARCHAR(20)        NULL,
+	PROVIDER                 VARCHAR(300)       NULL,   
+	TYPE                     VARCHAR(300)       NULL,  
+    DETAILS		             TEXT			    NULL,
+    REMARKS		             TEXT			    NULL
+);
